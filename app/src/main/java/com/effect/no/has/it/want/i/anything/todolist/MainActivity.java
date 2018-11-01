b2641e75af2217a6df63a2ad429b2aade8e26adf
Shinkojiro <james.a.hall@okstate.edu>
2018-10-31 19:05:42 -0500

    hmrr

--- app/src/main/java/com/effect/no/has/it/want/i/anything/todolist/MainActivity.java
+++ app/src/main/java/com/effect/no/has/it/want/i/anything/todolist/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+// a change
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
